0c0bb5a2cd2924625551dfa1bfd1223b6278c7e5
BigXavage <bigxavage@gmail.com>
2025-04-17 02:09:47 -0700

    Initial commit: Added website files with Formspree integration

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Theme Toggle
+document.querySelector('.theme-toggle').addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    const icon = document.querySelector('.theme-toggle i');
+    icon.classList.toggle('fa-moon');
+    icon.classList.toggle('fa-sun');
+});
